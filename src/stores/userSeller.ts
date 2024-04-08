aeb2ce9121d1f76ecfa7cb3a47d12e14a0f345e2
ridwanNurfauzi <mrnhp123@gmail.com>
2024-04-08 16:25:21 +0700

    added User Seller Store

---
+++ src/stores/userSeller.ts
@@ -0,0 +1,78 @@
+import axios, { type AxiosRequestConfig } from 'axios';
+import { defineStore } from 'pinia'
+import type { VueCookies } from 'vue-cookies';
+import { useConfigStore } from "./_config";
+
+
+declare global {
+    interface Window {
+        $cookies: VueCookies;
+    }
+}
+
+export const useUserSellerStore = defineStore('user_seller', {
+    state() {
+        const seller: any = null;
+
+        return {
+            seller
+        };
+    },
+    getters: {
+        _config: () => useConfigStore()
+    },
+    actions: {
+        async fetchSellerByCode(code: any) {
+            try {
+                let config: AxiosRequestConfig = {};
+                if (window.$cookies.isKey('userToken')) {
+                    config.headers = {
+                        "Authorization": `Bearer ${window.$cookies.get('userToken')}`
+                    }
+                }
+                const res = await axios.get(
+                    `${this._config.API_URL}/user/sellers/code/${code}`,
+                    config
+                );
+
+                if (res.data.success) {
+                    this.$state.seller = res.data;
+                    return true;
+                }
+                else {
+                    this.$state.seller = null;
+                    return false;
+                }
+            } catch (error) {
+                console.log(error);
+                return false;
+            }
+        },
+        async subscribe(seller_id: any) {
+            try {
+                let config: AxiosRequestConfig = {};
+                if (window.$cookies.isKey('userToken')) {
+                    config.headers = {
+                        "Authorization": `Bearer ${window.$cookies.get('userToken')}`,
+                        "Content-Type": 'multipart/form-data'
+                    }
+                }
+                const res = await axios.post(
+                    `${this._config.API_URL}/user/sellers/subscribe`,
+                    { seller_id },
+                    config
+                );
+
+                if (res.data.success) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            } catch (error) {
+                console.log(error);
+                return false;
+            }
+        }
+    }
+});
