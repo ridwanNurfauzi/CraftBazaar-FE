31afff00a85d3466a84fe78e0435cf4aa5bea8cc
ridwanNurfauzi <mrnhp123@gmail.com>
2024-04-22 08:42:43 +0700

    added fetchSellers function

--- src/stores/userSeller.ts
+++ src/stores/userSeller.ts
@@ -13,15 +13,41 @@ declare global {
 export const useUserSellerStore = defineStore('user_seller', {
     state() {
         const seller: any = null;
+        const sellers: any = null;
 
         return {
-            seller
+            seller,
+            sellers
         };
     },
     getters: {
         _config: () => useConfigStore()
     },
     actions: {
+        async fetchSellers() {
+            try {
+                let config: AxiosRequestConfig = {};
+                if (window.$cookies.isKey('userToken')) {
+                    config.headers = {
+                        "Authorization": `Bearer ${window.$cookies.get('userToken')}`
+                    }
+                }
+                const res = await axios.get(
+                    `${this._config.API_URL}/user/sellers`,
+                    config
+                );
+                if (res.data.success) {
+                    this.$state.sellers = res.data;
+                    return true;
+                } else {
+                    this.$state.sellers = null;
+                    return false;
+                }
+            } catch (error) {
+                console.log(error);
+                return false;
+            }
+        },
         async fetchSellerByCode(code: any) {
             try {
                 let config: AxiosRequestConfig = {};
