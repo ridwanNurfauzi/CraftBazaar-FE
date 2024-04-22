1837c6ca826fd7f9daca8111b8657bee50a538be
ridwanNurfauzi <mrnhp123@gmail.com>
2024-04-22 08:43:56 +0700

    added search products

--- src/views/user/ProductsView.vue
+++ src/views/user/ProductsView.vue
@@ -5,7 +5,16 @@
                 <h1 class="text-center text-lg font-medium">Produk</h1>
             </div>
             <div class="flex mx-4">
-                <div class="ms-auto">
+                <div class="ms-auto flex">
+                    <div class="relative me-2">
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <i class="bi bi-search text-gray-500"></i>
+                        </div>
+                        <input type="text" id="simple-search" v-model="searchTxt"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5"
+                            placeholder="Cari . . ." />
+                    </div>
+
                     <div>
                         <select v-model="sortMenu" id="sortMenu" @change="changeSortMenu()"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
@@ -17,7 +26,7 @@
                 </div>
             </div>
             <div class="flex flex-wrap justify-center m-4">
-                <div class="m-3" v-for="product in products?.data ?? []">
+                <div class="m-3" v-for="product in getProducts">
                     <RouterLink :to="{ name: 'user.productBySlug', params: { slug: product.slug } }">
                         <ProductCard :product="product"></ProductCard>
                     </RouterLink>
@@ -36,7 +45,8 @@ import ProductCard from "@/components/user/ProductCard.vue";
 export default {
     data(vm) {
         return {
-            sortMenu: 'latest'
+            sortMenu: 'latest',
+            searchTxt: ''
         }
     },
     components: {
@@ -64,7 +74,16 @@ export default {
         }
     },
     computed: {
-        ...mapState(useUserProductStore, ['products', 'latestProducts', 'earliestProducts', 'popularProducts']),
+        ...mapState(useUserProductStore, ['products']),
+        getProducts() {
+            return (this.products?.data ?? []).filter((e: any) => {
+                return (
+                    (e.name as string).toLowerCase().includes(this.searchTxt.toLowerCase()) ||
+                    (e.seller.name as string).toLowerCase().includes(this.searchTxt.toLowerCase())
+                )
+            }
+            );
+        }
     },
     async mounted() {
         await this.changeSortMenu();
